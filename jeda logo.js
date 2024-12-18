36649baa4ccbcab3fe68b0c709181fb5107e2f3e
CpmJhon21 <cpmjho@gmail.com>
2024-12-18 22:49:32 +0700

    jeda logo.js

--- jeda logo.js
+++ jeda logo.js
@@ -2,11 +2,11 @@
 setTimeout(() => {
     const splash = document.getElementById('splash');
     splash.classList.add('visible'); // Tampilkan splash screen
-}, 500000); // Jeda 10 detik
+}, 4000); // Jeda 10 detik
 
 // Jeda untuk menghilangkan splash screen setelah 40 detik dari saat muncul
 setTimeout(() => {
     const splash = document.getElementById('splash');
     splash.classList.remove('visible'); // Sembunyikan splash screen
     splash.classList.add('hidden');    // Pastikan tidak muncul lagi
-}, 10000); // 50 detik (10 detik untuk muncul + 40 detik aktif)
+}, 4000); // 50 detik (10 detik untuk muncul + 40 detik aktif)
