8fe3d047f1a59ecc6192a5a9dda35a19a04d7ce4
CpmJhon21 <cpmjho@gmail.com>
2024-12-20 18:47:51 +0700

    Delete public/js/musik.js

--- public/js/musik.js
+++
@@ -1,7 +0,0 @@
-const backgroundMusic = document.getElementById("background-music");
-
-// Play music when the page is loaded
-window.addEventListener("load", () => {
-    backgroundMusic.play().catch((error) => {
-        console.log("Autoplay was prevented by the browser. Music requires user interaction.");
-    });
