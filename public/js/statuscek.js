9b3c39d1c2183545933702661b0aff671ee3d198
CpmJhon21 <cpmjho@gmail.com>
2025-02-21 03:42:23 +0700

    statuscek.js

--- public/js/statuscek.js
+++ public/js/statuscek.js
@@ -1,18 +1,2 @@
-window.addEventListener("online", () => {
-    alert("Kamu kembali online ✅");
-localStorage.setItem("lastPage", window.location.href);
-    setTimeout(() => {
-        localStorage.setItem("cpm-jhon.my.id", true);
-        window.location.href = "https://cpm-jhon.my.id";
-    }, 5000);
-    }
-});
-
-window.addEventListener("offline", () => {
-    alert("Kamu sedang offline ❌");
-    localStorage.setItem("lastPage", window.location.href);
-    setTimeout(() => {
-        localStorage.setItem("redirectedToDino", true);
-        window.location.href = "https://elgoog.im/dinosaur-game/";
-    }, 5000);
-});
+window.addEventListener("online", () => alert("Kamu kembali online ✅"));
+window.addEventListener("offline", () => alert("Kamu sedang offline ❌"));
