5070f3cdb4a4f4fd0215de31e2e7e7c32c28d37d
CpmJhon21 <cpmjho@gmail.com>
2025-02-21 10:35:01 +0700

    sw.js

--- public/js/sw.js
+++ public/js/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "offline-cache-v1";
-const OFFLINE_URL = "/offline.html";
+const OFFLINE_URL = "offline.html";
 
 // Instal Service Worker dan simpan halaman offline ke cache
 self.addEventListener("install", (event) => {
