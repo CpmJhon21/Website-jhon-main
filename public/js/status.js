67548c74bd623bbac8009252204df095c089a686
CpmJhon21 <cpmjho@gmail.com>
2025-02-21 11:07:22 +0700

    status.js

--- public/js/status.js
+++ public/js/status.js
@@ -1,7 +1,13 @@
 function cekKoneksi() {
             if (navigator.onLine) {
-                window.location.href = "/";
+                window.location.href = "/"; // Jika online, kembali ke halaman utama
             } else {
                 alert("Masih offline. Periksa koneksi Anda.");
             }
-}
+        }
+
+        // Jika kembali online, otomatis redirect ke halaman utama
+        window.addEventListener("online", () => {
+            alert("Koneksi kembali! Kembali ke website...");
+            window.location.href = "/";
+        });
