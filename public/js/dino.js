8f4820231af89894eb559b4e06d9d0ca7ebc3bf9
CpmJhon21 <cpmjho@gmail.com>
2025-03-09 14:49:44 +0700

    dino.js

--- public/js/dino.js
+++ public/js/dino.js
@@ -1,17 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let dino = document.querySelector(".dino-container");
-    let bird = document.querySelector(".bird-container");
+    let dinoContainer = document.querySelector(".dino-container");
 
     function checkScroll() {
-        let dinoPosition = dino.getBoundingClientRect().top;
-        let birdPosition = bird.getBoundingClientRect().top;
+        let dinoPosition = dinoContainer.getBoundingClientRect().top;
         let windowHeight = window.innerHeight;
 
-        if (dinoPosition < windowHeight) {
-            dino.classList.add("active");
-        }
-        if (birdPosition < windowHeight) {
-            bird.classList.add("active");
+        if (dinoPosition < windowHeight - 100) {
+            dinoContainer.classList.add("active"); // Mulai animasi
+            window.removeEventListener("scroll", checkScroll); // Hentikan event agar tidak terus dipanggil
         }
     }
 
