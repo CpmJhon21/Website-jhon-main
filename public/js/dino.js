ec9a8a2b9d985ecd806ae7e1b226355abb57870b
CpmJhon21 <cpmjho@gmail.com>
2025-03-09 13:41:00 +0700

    dino.js

--- public/js/dino.js
+++ public/js/dino.js
@@ -1,28 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     let dinoContainer = document.querySelector(".dino-container");
-    let lastScrollY = window.scrollY;
 
     function checkScroll() {
         let dinoPosition = dinoContainer.getBoundingClientRect().top;
         let windowHeight = window.innerHeight;
-        let currentScrollY = window.scrollY;
 
         if (dinoPosition < windowHeight - 100) {
-            dinoContainer.classList.add("show");
-
-            if (currentScrollY > lastScrollY) {
-                dinoContainer.classList.remove("move-left");
-                dinoContainer.classList.add("move-right");
-            } else {
-                dinoContainer.classList.remove("move-right");
-                dinoContainer.classList.add("move-left");
-            }
-        } else {
-            dinoContainer.classList.remove("show");
+            dinoContainer.classList.add("active"); // Mulai animasi
+            window.removeEventListener("scroll", checkScroll); // Hentikan event agar tidak terus dipanggil
         }
-
-        lastScrollY = currentScrollY;
     }
 
-    window.addEventListener("scroll", () => requestAnimationFrame(checkScroll));
+    window.addEventListener("scroll", checkScroll);
 });
